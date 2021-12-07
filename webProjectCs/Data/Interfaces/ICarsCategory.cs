e642cf9a0e1ec9a6932a87c88b8fe91b0c5dc997
Alexandr <caiiiia_podoplelov@mail.ru>
2021-12-08 00:15:25 +0300

    Create interfaces

---
+++ webProjectCs/Data/Interfaces/ICarsCategory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webProjects.Data.Interfaces
+{
+    interface ICarsCategory
+    {
+    }
+}
--- webProjectCs/Data/Models/Category.cs
+++ webProjectCs/Data/Models/Category.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace webProjects.Data.Models
-{
-    public class Category
-    {
+namespace webProjects.Data.Models {
+    public class Category {
+        public int id { set; get; }
+        public int categoryName { set; get; }
+        public int desc { set; get; }
+        public int id { set; get; }
     }
 }
